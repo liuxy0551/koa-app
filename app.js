1e34824aae397f2d0385d93d792d9ae41a26deff
liuyi <liuxy0551@qq.com>
2020-02-08 22:58:34 +0800

    add: http.get(), http.post()

--- app.js
+++ app.js
@@ -1,15 +1,29 @@
 // 引入模块
 const Koa = require('koa')
 const Router = require('koa-router')
+const bodyParser = require('koa-bodyparser')
 
 // 实例化
 const app = new Koa()
 const router = new Router()
 
-// 路由
-router.get('/', async ctx => {
-  ctx.body = { msg: 'Hello Koa Interfaces' }
+app.use(async (ctx, next) => {
+  ctx.body = { code: 200, message: '成功' }
+  await next()
 })
+
+// 配置中间件，通过 bodyParser 获取 post 请求传递过来的参数
+app.use(bodyParser())
+
+// 路由
+router
+  .get('/hello', async ctx => {
+    let { q } = ctx.query
+    Object.assign(ctx.body, { data: { msg: 'Hello Koa Interfaces', q }})
+  })
+  .post('/say', async ctx => {
+    Object.assign(ctx.body, { data: ctx.request.body })
+  })
 
 // 配置路由
 app.use(router.routes()).use(router.allowedMethods())
